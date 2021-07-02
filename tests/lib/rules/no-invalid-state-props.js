6f7da292cca3b2f6bb39717d5c82e5dbfdae1e8c
Richard Laffers <rlaffers@gmail.com>
2021-07-02 17:37:50 +0200

    fix(no-invalid-state-props): allow "strict" and "data" state props

--- tests/lib/rules/no-invalid-state-props.js
+++ tests/lib/rules/no-invalid-state-props.js
@@ -7,6 +7,7 @@ const tests = {
       createMachine({
         context: {}, // valid in the root node
         initial: 'idle',
+        strict: true,
         states: {
           idle: {
             type: 'parallel',
@@ -36,6 +37,10 @@ const tests = {
               },
               writing: {},
             },
+          },
+          done: {
+            type: 'final',
+            data: {},
           },
         },
       })
